b796dcff214326c50503107a4575110585c9ffed
Timothy Mugo Gachengo <timomugo01@gmail.com>
2024-04-13 11:23:48 +0300

    added launch screen and app icon

--- todolist/ViewModels/ProfileViewModel.swift
+++ todolist/ViewModels/ProfileViewModel.swift
@@ -7,19 +7,16 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 
 class ProfileViewModel: ObservableObject {
     init () {}
     
+    @Published var user: User? = nil
+    
     func signOut () {
-        guard let uid = Auth.auth().currentUser?.uid else {
-            do {
-                try Auth.auth().signOut()
-            } catch {
-                return
-            }
-            
+        guard (Auth.auth().currentUser?.uid) != nil else {
             return
         }
         
@@ -28,6 +25,34 @@ class ProfileViewModel: ObservableObject {
         } catch {
             return;
         }
+        
+        
+    }
+    
+    func getUser () {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uid)
+            .getDocument {
+                [weak self] snapshot, error in
+                guard let data = snapshot?.data(), error == nil else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self?.user = User(
+                        id: data["id"] as? String ?? "",
+                        name: data["name"] as? String ?? "",
+                        email: data["email"] as? String ?? "",
+                        joined: data["joined"] as? TimeInterval ?? 0)
+                }
+                
+            }
         
         
     }
--- todolist/Views/ProfileView.swift
+++ todolist/Views/ProfileView.swift
@@ -13,16 +13,57 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
-                
-                // sign out button
-                TLButton(title: "Sign Out", background: .blue) {
-                    viewModel.signOut()
+                if let user = viewModel.user {
+                   profile(user: user)
+                } else {
+                    Text("Loading User....")
                 }
+                
+                
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.getUser()
+        }
     }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            . foregroundColor(.blue)
+            .frame(width: 125, height: 125)
+        
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                    .padding()
+                Text(user.name)
+            }
+            HStack {
+                Text("Email: ")
+                    .bold()
+                    .padding()
+                Text(user.email)
+            }
+            HStack {
+                Text("Created At: ")
+                    .bold()
+                    .padding()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+        }
+        
+        // sign out button
+        Button("Sign Out") {
+            viewModel.signOut()
+        }.foregroundColor(.red)
+    }
+    
 }
 
 #Preview {
